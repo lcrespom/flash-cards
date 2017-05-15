f0a85e11b827ecaea1ede266174761a096adddb5
Luis Crespo <lcrespo@es.ibm.com>
2017-05-15 16:53:40 +0200

    Refactored game buttons

---
+++ src/components/GameButtons.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="fc-game-buttons">
+		<div class="fc-button-row">
+			<button class="fc-game-btn btn btn-primary">
+				<div class="fc-game-turn">↩️</div>
+			</button>
+		</div>
+		<div class="fc-button-row">
+			<button class="fc-game-btn btn btn-success">
+				<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+			</button>
+			<button class="fc-game-btn btn btn-danger">
+				<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+			</button>
+			<button class="fc-game-btn btn btn-warning">
+				<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'game-buttons'
+};
+</script>
+
+<style scoped>
+.fc-game-buttons {
+	position: absolute;
+	bottom: 10px;
+	left: 0; right: 0;
+}
+.fc-button-row {
+	display: flex;
+	justify-content: space-around;
+	margin-top: 15px;
+}
+.fc-game-btn {
+	width: 50px;
+	height: 50px;
+	padding: 0;
+}
+.fc-game-btn > span {
+	font-size: 170%;
+}
+.fc-game-turn {
+  font-size: 300%;
+  margin-top: -6px;
+}
+</style>
--- src/components/Play.vue
+++ src/components/Play.vue
@@ -7,62 +7,24 @@
 		</div>
 		<div class="fc-card">
 		</div>
-		<div class="fc-game-buttons">
-			<div class="fc-button-row">
-				<button class="fc-game-btn btn btn-primary">
-					<div class="fc-game-turn">↩️</div>
-				</button>
-			</div>
-			<div class="fc-button-row">
-				<button class="fc-game-btn btn btn-success">
-					<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-				</button>
-				<button class="fc-game-btn btn btn-danger">
-					<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-				</button>
-				<button class="fc-game-btn btn btn-warning">
-					<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-				</button>
-			</div>
-		</div>
+		<game-buttons />
 	</div>
 </template>
 
 <script>
+import GameButtons from './GameButtons';
+
 export default {
-	name: 'play'
+	name: 'play',
+	components: { GameButtons }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .fc-card {
 	position: absolute;
 	top: 25px; left: 10px; bottom: 140px; right: 10px;
 	border: 2px solid #888;
 	border-radius: 15px;
-}
-
-.fc-game-buttons {
-	position: absolute;
-	bottom: 10px;
-	left: 0; right: 0;
-}
-.fc-button-row {
-	display: flex;
-	justify-content: space-around;
-	margin-top: 15px;
-}
-.fc-game-btn {
-	width: 50px;
-	height: 50px;
-	padding: 0;
-}
-.fc-game-btn > span {
-	font-size: 170%;
-}
-.fc-game-turn {
-  font-size: 300%;
-  margin-top: -6px;
 }
 </style>
