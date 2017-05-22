0741240ecf37e7fa941635ff0d20817980821e24
Luis Crespo <lcrespo@es.ibm.com>
2017-05-22 16:26:07 +0200

    Settings

--- README.md
+++ README.md
@@ -4,19 +4,22 @@
 
 ## ToDo
 - Pages:
-	- Settings:
+	- Home:
+		- Link to settings, search, new card set
+	- Settings (from home):
 		- Shuffle y/n (checkbox)
 		- Card timer (Select with No limit, 5, 10, 15, 20, 30, 40, 50, 60 seconds)
 		- Show card front / show card back (radio)
-	- Play game
-	- Game results:
+	- Play game (from search)
+		- at end of game, display game results page
+	- Game results (from play game):
 		- Show results: number of OK, fail, ignored
 		- Show score
 			- Rule: OK: +1, fail: -1, ignore: 0
 		- Show stats progression (NTH)
 		- Home button
 		- Repeat button
-	- Search cards
-		- Search, select => play
-	- New card set
-	- Edit card set
+	- Search cards (from home)
+		- Search, select => edit / play
+	- New card set (from home)
+	- Edit card set (from search)
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,6 +1,54 @@
 <template>
-	<div>
-		ToDo: settings
+	<div class="fc-fill">
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="fc-title navbar-brand">Settings</div>
+		</nav>
+		<form class="fc-content">
+			<div class="form-group fc-group">
+				<label for="settings-shuffle">Shuffle</label>
+				<div class="checkbox" id="settings-shuffle"  style="margin-top: 0">
+					<label>
+						<input type="checkbox">Shuffle cards
+					</label>
+				</div>
+			</div>
+
+			<div class="form-group fc-group">
+				<label for="settings-timer">Time per card</label>
+				<select class="form-control" id="settings-timer">
+					<option value="5">5 s</option>
+					<option value="5">10 s</option>
+					<option value="15">15 s</option>
+					<option value="20">20 s</option>
+					<option value="30">30 s</option>
+					<option value="40">40 s</option>
+					<option value="50">50 s</option>
+					<option value="60">60 s</option>
+					<option value="0">No time limit</option>
+				</select>
+			</div>
+
+			<div class="form-group fc-group">
+				<label>Which side to show</label>
+				<div class="radio" style="margin-top: 0">
+					<label>
+						<input type="radio" name="frontback" value="front" checked>
+						Show card front
+					</label>
+				</div>
+				<div class="radio">
+					<label>
+						<input type="radio" name="frontback" value="back">
+						Show card back
+					</label>
+				</div>
+			</div>
+
+			<div class="form-group fc-group fc-btn-row">
+				<button class="btn btn-default">Cancel</button>
+				<button class="btn btn-primary">Save</button>
+			</div>
+		</form>
 	</div>
 </template>
 
@@ -11,4 +59,23 @@ export default {
 </script>
 
 <style scoped>
+.fc-title {
+	width: 100%;
+	text-align: center;
+	color: #444;
+}
+.fc-content {
+	padding: 60px 10px 10px;
+}
+.fc-group {
+	margin-top: 30px;
+}
+.fc-btn-row {
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+}
+.fc-btn-row > button {
+	width: 5em;
+}
 </style>
